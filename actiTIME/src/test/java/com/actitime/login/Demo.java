277e9d5073d3ea61e7a51ddf219c8454fa0a9214
Charu Gupta <Charu Gupta@192.168.1.5>
2020-07-02 11:44:54 +0530

    Actitime commit

---
+++ actiTIME/src/test/java/com/actitime/login/Demo.java
@@ -0,0 +1,13 @@
+package com.actitime.login;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+	public void login()
+	{
+		Reporter.log("script is running successfully",true);
+	}
+}
