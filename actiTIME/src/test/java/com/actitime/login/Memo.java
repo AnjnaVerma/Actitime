2fe4d1a76e345918f757a46cf33c0f7d88213553
Charu Gupta <Charu Gupta@192.168.1.5>
2020-07-02 12:28:57 +0530

    Memo commit2

--- actiTIME/src/test/java/com/actitime/login/Memo.java
+++ actiTIME/src/test/java/com/actitime/login/Memo.java
@@ -1,10 +1,11 @@
 package com.actitime.login;
 
 import org.testng.Reporter;
+import org.testng.annotations.Test;
 
 public class Memo {
 
-	
+	@Test
 	public void logout()
 	{
 		Reporter.log("Logout successfully", true);
