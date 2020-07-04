1c9d312a1e5da45dc0ba634e620a2f3ff2d15db2
Charu Gupta <Charu Gupta@192.168.1.5>
2020-07-04 08:59:48 +0530

    commit jenkins

---
+++ actiTIME/src/test/java/com/actitime/login/Remo.java
@@ -0,0 +1,13 @@
+package com.actitime.login;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Remo {
+
+	@Test
+	public void run()
+	{
+		Reporter.log("Run", true);
+	}
+}
